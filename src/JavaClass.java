9b0f0ba03e47ff2a01dc0161aa442b7dc3e7a75b
Arhan <arhanh@nycstudents.net>
2022-10-15 18:27:51 -0400

    Completed

---
+++ src/JavaClass.java
@@ -0,0 +1,10 @@
+public class JavaClass {
+    public static void main(String [] args){
+
+        int answer1 = Math.multiplyExact(12,5);
+        int answer2 = Math.multiplyExact(2,4);
+        System.out.println(answer1);
+        System.out.println(answer2);
+
+    }
+}
---
+++ src/LuckyNumbersRunner.java
@@ -0,0 +1,13 @@
+public class LuckyNumbersRunner {
+    static public void main(String [] args){
+
+        LuckyNumbers num1 = new LuckyNumbers();
+        String luckyNumbers = num1.getLuckyNumbers();
+        System.out.println(luckyNumbers);
+
+
+
+
+
+    }
+}
